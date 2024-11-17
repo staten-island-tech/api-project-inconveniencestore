40e25dabcba2b1dbd6caf2cf1581f1bc7be26448
inconveniencestore <iivyleaves@gmail.com>
2024-11-16 22:17:34 -0500

    arcane ruined me

--- app/main.js
+++ app/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+async function getData() {
+  try {
+    const response = await fetch("http://api.zippopotam.us/us/90210");
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sorry couldn't find that pocket monster :(");
+  } //await only works in async functions. it wil move on to next section even without a response.
+}
+getData();

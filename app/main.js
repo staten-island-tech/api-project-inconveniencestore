5998adda298fc38788a3cecf1d127553f6e16c15
inconveniencestore <iivyleaves@gmail.com>
2024-11-18 12:26:48 -0500

    ok new links :3 we got an idea :3

--- app/main.js
+++ app/main.js
@@ -1,17 +1,25 @@
 import "./style.css";
+
+//useless facts about places given ip?? or something
+
 async function getData() {
   try {
-    const response = await fetch("http://api.zippopotam.us/us/90210");
+    const response = await fetch(
+      "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+    );
+    //https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+    //http://api.zippopotam.us/us/10306
+    //https://github.com/robertoduessmann/weather-api
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
       console.log(data);
-      document.querySelector("h1").textContent = data.name;
+      document.querySelector("h1").textContent = data.results.sunset;
     }
   } catch (error) {
     console.log(error);
-    console.log("sorry couldn't find that pocket monster :(");
-  } //await only works in async functions. it wil move on to next section even without a response.
+    console.log("ERROR");
+  }
 }
 getData();

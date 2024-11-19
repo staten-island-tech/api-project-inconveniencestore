cd24a177c3f96d593578a404e5833b986b5585ea
inconveniencestore <iivyleaves@gmail.com>
2024-11-19 12:27:35 -0500

    i am insanely unproductive

--- app/main.js
+++ app/main.js
@@ -2,6 +2,8 @@ import "./style.css";
 
 //useless facts about places given ip?? or something
 
+const DOM = {};
+
 async function getData() {
   try {
     const response = await fetch(
@@ -9,13 +11,13 @@ async function getData() {
     );
     //https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
     //http://api.zippopotam.us/us/10306
-    //https://github.com/robertoduessmann/weather-api
+    //https://github.com/robertoduessmann/weather-apihttps://github.com/robertoduessmann/weather-api
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
       console.log(data);
-      document.querySelector("h1").textContent = data.results.sunset;
+      document.querySelector(".title").textContent = data.results.sunset;
     }
   } catch (error) {
     console.log(error);

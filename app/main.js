a53052b8e45f4e838591b15a2158aa7ea560a480
inconveniencestore <iivyleaves@gmail.com>
2024-11-20 12:29:32 -0500

    kiall me

--- app/main.js
+++ app/main.js
@@ -1,17 +1,25 @@
 import "./style.css";
 
-//useless facts about places given ip?? or something
+//ask for ip
 
-const DOM = {};
+const DOMSelectors = { holder: document.querySelector(".holder") };
 
-async function getData() {
+function createItems(items) {
+  DOMSelectors.holder.innerHTML = "";
+  items.forEach((value) =>
+    DOMSelectors.holder.insertAdjacentHTML("beforeend", ``)
+  );
+}
+
+async function getData(lat, lng) {
   try {
     const response = await fetch(
-      "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+      //"https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + lng
+      "http://api.zippopotam.us/us/10306"
     );
-    //https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
-    //http://api.zippopotam.us/us/10306
-    //https://github.com/robertoduessmann/weather-apihttps://github.com/robertoduessmann/weather-api
+    //https://api.sunrise-sunset.org/json?lat=${lat}&lng=-4.4203400
+    //http://api.zippopotam.us/us/10306 can get name, place, long and lat given ip address
+    //https://github.com/robertoduessmann/weather-api
     if (response.status != 200) {
       throw new Error(response);
     } else {

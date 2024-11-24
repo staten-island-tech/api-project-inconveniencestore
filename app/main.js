f4d7aba48447d9f091665fe94f8fac8af769e244
inconveniencestore <iivyleaves@gmail.com>
2024-11-24 17:34:49 -0500

    we're getting somewhere. idk where tho

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 //ask for country, then zip code?
 //ask for zip code
+//try these zip codes and 3 random number
 
 //when search bar clicked, then summon get data
 
@@ -9,9 +10,44 @@ const DOMSelectors = {
   holder: document.querySelector(".holder"),
   submit: document.querySelector(".submit"),
   placeholder: document.querySelector(".title"),
+  zipcode: document.querySelector("#zipcode"),
 };
 
 DOMSelectors.submit.addEventListener("click", createItems);
+
+async function checkTheseOut() {
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function createDemoZipcards(items) {
+    const place = items.places[0];
+
+    DOMSelectors.holder.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card bg-red-500 flex flex-col items-center justify-around text-center p-12 rounded-lg w-80 max-w-lg h-80 m-8">
+        <h2 class="text-3xl">Place Name: ${place["place name"]}</h1>
+          <h3 class="text-xl">State: ${place.state}</h3>
+          <h3 class="text-xl">Coordinates: (${place.longitude}, ${place.latitude})</h3>
+          <h4 class="text-lg">Zip Code: ${items["post code"]}</h4>
+      </div>`
+    );
+  }
+  let infoAcquired = false;
+  while (infoAcquired === false) {
+    const randomPlace = getRandomInt(10000, 99950);
+    console.log(randomPlace);
+
+    try {
+      const items = await getData(randomPlace);
+      createDemoZipcards(items);
+      infoAcquired = true;
+    } catch (error) {
+      console.error("createitems error", error);
+      DOMSelectors.holder.textContent = "error, trying again";
+    }
+  }
+}
+checkTheseOut();
 
 async function getData(zipcode) {
   try {
@@ -21,8 +57,8 @@ async function getData(zipcode) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      console.log(data.country);
-      return data.country;
+      console.log(data);
+      return data;
     }
   } catch (error) {
     console.log(error);
@@ -33,24 +69,19 @@ async function getData(zipcode) {
 async function createItems() {
   DOMSelectors.holder.innerHTML = "";
 
+  const zipcode = DOMSelectors.zipcode.value;
   try {
-    const items = await getData("10306");
+    const items = await getData(zipcode);
+    const place = items.places[0];
+
     DOMSelectors.holder.insertAdjacentHTML(
       "beforeend",
-      `<div class="card bg-red-600 flex flex-col items-center justify-around text-center p-12 rounded-lg w-80 max-w-lg h-80 m-8">
-        <h1 class="text-3xl">${items}</h1>
-        <h2 class="text-xl">${items}</h2>
+      `<div class="card bg-red-500 flex flex-col items-center justify-around text-center p-12 rounded-lg w-80 max-w-lg h-80 m-8">
+        <h2 class="text-3xl">Place Name : ${place["place name"]}</h1>
+        <h3 class="text-xl">State: ${place.state}</h3>
+        <h3 class="text-xl">coordinates: (${place.longitude}, ${place.latitude})</h2>
       </div>`
     );
-    /* items.forEach((item) =>
-      DOMSelectors.holder.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card bg-red-600 flex flex-col items-center justify-around text-center p-12 rounded-lg w-80 max-w-lg h-80 m-8">
-          <h1 class="text-3xl">${item.title}</h1>
-          <h2 class="text-xl">${item.subtitle}</h2>
-        </div>`
-      )
-    ); */
   } catch (error) {
     console.error("createitems error", error);
     DOMSelectors.holder.textContent = "ERROR YOU SUCK AT CODING HAHAHHAHA";

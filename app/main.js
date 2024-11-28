58cf564d2e791188aea57b6fdc9e7912ddf48e62
inconveniencestore <iivyleaves@gmail.com>
2024-11-28 18:44:53 -0500

    thank god the button finally works
    - need to fix go back button

--- app/main.js
+++ app/main.js
@@ -43,7 +43,6 @@ async function createItems() {
 
 async function getData(zipcode) {
   try {
-    console.log(`getting data for: http://api.zippopotam.us/us/${zipcode}`);
     const response = await fetch(`http://api.zippopotam.us/us/${zipcode}`);
     if (response.status != 200) {
       throw new Error("getData error: ", response);
@@ -119,19 +118,64 @@ function defaultSetup() {
 function createCards(selection, items) {
   const element = DOMSelectors[selection];
   const place = items.places[0];
+
   element.insertAdjacentHTML(
     "beforeend",
     `<div class="card bg-red-500 flex flex-col items-center justify-around text-center p-12 rounded-lg w-80 max-w-lg h-80 m-8">
         <h2 class="text-3xl">Place Name: ${place["place name"]}</h1>
         <h3 class="text-xl">State: ${place.state}</h3>
         <h4 class="text-lg">Zip Code: ${items["post code"]}</h4>
-        <button class="hooray bg-blue-300" 
-                data-zipcode="${items["post code"]}" 
-                data-coordinates="${place.longitude},${place.latitude}">
+        <button class="hooray bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                data-zipcode="${items["post code"]}" >
           Select
         </button>
       </div>`
   );
+}
+
+async function putTheSecondCardOntoThePage(zipcode) {
+  DOMSelectors.body.innerHTML = "";
+  try {
+    const items = await getData(zipcode);
+    const place = items.places[0];
+
+    try {
+      const response = await fetch(
+        `https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400`
+      );
+      const data = await response.json();
+      console.log(data);
+      DOMSelectors.body.insertAdjacentHTML(
+        "beforeend",
+        `<div class="holder flex items-center justify-center">
+      <div class="flex items-center justify-center h-screen w-screen">
+        <div class="selected-info bg-red-500 flex flex-col items-center justify-around text-center p-12 rounded-lg w-90 max-w-none h-90 m-0">
+          <h3 class="text-3xl">SELECTED ZIPCODE: ${zipcode}</h3>
+          <h3 class="text-2xl">name of place: ${place["place name"]}</h3>
+          <ul>
+            <li>sunrise time: ${data.results.sunrise}</li>
+            <li>sunset time: ${data.results.sunset}</li>
+            <li>day length:  ${data.results.day_length}</li>
+            <li>solar noon:  ${data.results.solar_noon}</li>
+            <li>nautical twilight begins at: ${data.results.nautical_twilight_begin}</li>
+            <li>nautical twilight ends at: ${data.results.nautical_twilight_end}</li>
+            <li>civil twilight begins at: ${data.results.civil_twilight_begin}</li>
+            <li>civil twilight ends at: ${data.results.civil_twilight_end}</li>
+          </ul>
+          <ul>
+            <li>longitude & latitude: ${place.longitude}, ${place.latitude}</li>
+            <li>state: ${place.state}</li>
+          </ul>
+          <button type="submit" class="go-back bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">go back</button>
+        </div>
+      </div>`
+      );
+    } catch {
+      console.log("sunset sunrise error");
+    }
+  } catch (error) {
+    console.error("create items error", error);
+  }
 }
 
 //not only adds button listeners but also adds adjacent body html.
@@ -143,30 +187,7 @@ function attachButtonListeners(items) {
     //thing that happens when clicked
     button.addEventListener("click", (event) => {
       const zipcode = event.target.getAttribute("data-zipcode");
-      const coordinates = event.target.getAttribute("data-coordinates");
-      // smite: this only takes the latest information.
-      //what i need to do is to put this into a seperate function and then cry! no jk call it seperately, return zipcode and coordinates in an array? or just coordinates? or the whole array??? probaby whole array actuall
-      //upon further research the data thingie can only store strings which is soooo lame (joke)
-      //new plan: store zipcode, derive everythign else from there
-      DOMSelectors.body.innerHTML = "";
-      console.log(`zipcode selected: ${zipcode}, coordinates: ${coordinates}`);
-      DOMSelectors.body.insertAdjacentHTML(
-        "beforeend",
-        `<div class="holder flex items-center justify-center">
-      <div class="flex items-center justify-center h-screen w-screen">
-        <div class="selected-info bg-red-500 flex flex-col items-center justify-around text-center p-12 rounded-lg w-90 max-w-none h-90 m-0">
-          <h2 class="text-3xl">SELECTED ZIPCODE: ${items["post code"]}</h2>
-          <h3>name of place: ${place["place name"]}</h3>
-          <ul>
-            <li>sunset time: </li>
-            <li>sunrise time: </li>
-            <li>state: </li>
-            <li>longitude & latitude: </li>
-          </ul>
-          <button type="submit" class="go-back bg-blue-300">go back</button>
-        </div>
-      </div>`
-      );
+      putTheSecondCardOntoThePage(zipcode);
     });
   });
 }
